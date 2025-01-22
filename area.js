826bbd597e6d93b15bd574168987261f726e1a59
ahnaf-hasan <ahnaf.cse8741@gmail.com>
2025-01-23 02:44:11 +0600

    add new file

---
+++ area.js
@@ -0,0 +1,21 @@
+// Let's Start
+
+function triangleArea(base, height) {
+  
+  return 0.5 * base * height;
+
+}
+
+//Solution
+
+let baseLength = 25;
+
+let heightLength = 25;
+
+let area = triangleArea(baseLength, heightLength);
+
+console.log(
+
+  `Area of the triangle is: ${area}`
+
+);
