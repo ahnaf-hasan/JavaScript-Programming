14b55c2eae61af50a4c6a9eac319d0ac3746a1eb
ahnaf-hasan <ahnaf.cse8741@gmail.com>
2025-01-29 12:23:52 +0600

    add new file

---
+++ star.js
@@ -0,0 +1,21 @@
+//Let's Start
+function printStars(n) {
+    let stars = '';
+    for (let i = 0; i < n; i++) {
+      stars += '* ';
+    }
+    console.log(stars);
+  }
+  
+  //Set 
+
+  const rows = 3;
+  const cols = 3;
+  
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      printStars(3);
+    }
+    console.log('');
+  }
+  
