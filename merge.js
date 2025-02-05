568873d6c81bf4669b843db7e5905aee0056f8af
ahnaf-hasan <ahnaf.cse8741@gmail.com>
2025-02-05 12:14:48 +0600

    my fourteen commit

---
+++ merge.js
@@ -0,0 +1,35 @@
+//Let's Start
+function mergeSort(array) {
+    if (array.length <= 1) {
+        return array;
+    }
+
+    const middle = Math.floor(array.length / 2);
+    const left = array.slice(0, middle);
+    const right = array.slice(middle);
+
+    return merge(mergeSort(left), mergeSort(right));
+}
+
+//Function
+function merge(left, right) {
+    let result = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
+
+    while (leftIndex < left.length && rightIndex < right.length) {
+        if (left[leftIndex] < right[rightIndex]) {
+            result.push(left[leftIndex]);
+            leftIndex++;
+        } else {
+            result.push(right[rightIndex]);
+            rightIndex++;
+        }
+    }
+
+    return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+}
+
+// Example 
+const array = [1000,3000, 7000, 2000, 6000, 4000, 5000];
+console.log("Sorted array:", mergeSort(array));
