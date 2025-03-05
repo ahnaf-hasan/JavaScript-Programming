4808d18aefbb49d19455360c17a7ae31a3948c65
ahnaf-hasan <ahnaf.cse8741@gmail.com>
2025-03-05 21:56:24 +0600

    add new file

---
+++ add.js
@@ -0,0 +1,13 @@
+//Let's Start
+
+// Function to add two numbers
+function addNumbers(a, b) {
+    return a + b;
+  }
+  
+  // Calling the function and storing the result
+  let result = addNumbers(12, 13);
+  
+  // Displaying the result in the console
+  console.log("The sum is: " + result);
+  
