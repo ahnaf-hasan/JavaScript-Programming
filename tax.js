c4d310dfe85ac87eac7414582f0922b53da59e6f
ahnaf-hasan <ahnaf.cse8741@gmail.com>
2025-05-18 22:37:49 +0600

    update

--- tax.js
+++ tax.js
@@ -8,8 +8,8 @@ function calculateTax(income, expense) {
     return tax;
 }
 
-let income = 500000;  // Income value here
-let expense = 50000; // Expense value here
+let income = 1200000;  // Income value here
+let expense = 700000; // Expense value here
 
 //Result
 
